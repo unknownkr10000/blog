77a3264a867978b8cbec35d3d7d9071df27d82c8
Your Name <you@exmaple.com>
2018-02-20 21:04:05 +0900

    added login/logout system

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone 
+from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
@@ -16,3 +16,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __Str__(self):
+        return self.text
+
+    def approved_comment(self):
+        return self.comments.filter(approved_comment=True)
